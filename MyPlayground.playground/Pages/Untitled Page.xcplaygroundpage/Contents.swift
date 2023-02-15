a6a5a388daded758eeb0c32351aee724e91f881f
ruaasaad <100333163+ruaasaad@users.noreply.github.com>
2023-02-15 07:44:22 -0800

    My first commit

---
+++ MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,61 @@
+
+
+//Assignmant1
+//EX1
+var temperature: Float = 55.3
+print(temperature)
+
+
+//EX2
+
+let sec_per_min = 60
+print(sec_per_min)
+//sec_per_min = "50"
+print(sec_per_min)
+
+//EX4
+let carwheels:Int = 4
+print(carwheels)
+
+//EX5
+let π = 3.14
+print(π)
+
+//EX6
+var 😺 = "cat"
+print(😺)
+
+//EX7
+var car = "benz"
+print(car)
+//EX8
+//What type is the constant pi in the example below? Why?
+//let pi = 3 + 0.141592654
+//Pi is inferred to be of type Double
+//EX12
+// this is comment
+//EX13
+/* This is a comment
+over multiple lines. */
+
+//EX14
+print("Hello", terminator: " ")
+print("This is Ruaa")
+
+//EX15
+
+let hockey = ("Jeff", 33, "Ross", 54)
+print(hockey.0)
+print(hockey.1)
+print(hockey.2)
+print(hockey.3)
+
+//EX16
+
+let hockyName = ("Jeff", "Ross")
+let hockyNum = (33, 54)
+print(hockyName.0)
+print(hockyNum)
+
+
+
